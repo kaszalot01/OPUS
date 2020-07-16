e34d434c80a0f55823fb4bd570342092b4e2cc51
Brychlikow <brychlikow@gmail.com>
2020-07-16 19:39:18 +0200

    Clean up useless comments

--- opusparser.py
+++ opusparser.py
@@ -1,13 +1,3 @@
-#
-# This example demonstrates usage of the Indenter class.
-#
-# Since indentation is context-sensitive, a postlex stage is introduced to
-# manufacture INDENT/DEDENT tokens.
-#
-# It is crucial for the indenter that the NL_type matches
-# the spaces (and tabs) after the newline.
-#
-
 from lark import Lark
 from lark.indenter import Indenter
 
@@ -20,7 +10,6 @@ class TreeIndenter(Indenter):
     tab_len = 4
 
 parser = Lark.open("opuslang.lark", parser='lalr', debug=True, postlex=TreeIndenter())
-# parser = Lark(tree_grammar, postlex=TreeIndenter())
 
 test_tree = """
 @points > 21:
@@ -47,8 +36,6 @@ atom_test = " X 5 * Cpoints"
 def test():
     print(parser.parse(test_tree).pretty())
     print(parser.parse(test_tree))
-    # print(parser.parse(atom_test).pretty())
-    # print(parser.parse(atom_test))
 
 if __name__ == '__main__':
     test()
