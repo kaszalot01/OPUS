ed7659f3bbc2d5374b977d02e8f8104a8045160f
Brychlikow <brychlikow@gmail.com>
2020-07-27 19:05:36 +0200

    Add ir test independent of blas.ol

--- tests/test_ir.py
+++ tests/test_ir.py
@@ -15,6 +15,35 @@ def test_blas_parses():
             assert not isinstance(c, Tree)
 
 
+def test_simple_ir_parse():
+    test_system = \
+"""
+@points > 21:
+    bid 2♦
+
+    ♥ >= 4 and @points < 15:
+        bid 2♠
+    @points >= 15 and @points <= 17 and ♠ >= 2 and ♥ >= 2:
+        pass
+
+    $balance == 1 or $balance == 2:
+        @points >= 15 and @points <= 17:
+            bid 1NT
+
+            ((♥ == 4 or ♠ == 4) and @points >= 8) or (♥ >=4 and ♠ >= 4):
+                bid 2♣
+
+            ♣ >- 6 and 2 * ♣ + 0.5 * ♣points + @points >= 29.5 and @points < 15:
+                bid 4♥
+    else:
+        pass
+"""
+    system = System.parse_system(test_system)
+    for b in system.branches:
+        for c in b.children_iterator():
+            assert not isinstance(c, Tree)
+
+
 def test_blas_executes():
 
     here = pathlib.Path(__file__)
